300b04374ca3083628398799d0096c6c301781bb
christylsw <44832829+christylsw@users.noreply.github.com>
2021-05-11 21:53:08 +0800

    Add files via upload

---
+++ Backtesting Capital Guarantee Product vs Underlying Performance during COVID-19.R
@@ -0,0 +1,54 @@
+install.packages("xts")
+library("quantmod")
+
+#########################################################################################################
+#Write a Backtesting Function 
+#to compare the return of Capital Guarantee Note vs Underlying during COVID-19 (period: 2019-09-27 - 2020-03-27)
+backtesting_function <- function (stock_data){
+  notional <- 100
+  rf <- 0.0012
+  relative_price <- stock_data[[2,1]]/stock_data[[1,1]]*100
+
+  
+  underlying_price <- relative_price
+  underlying_price_return <- underlying_price/notional - 1
+
+  principal_guarantee_note_CF <- 100 + max((relative_price-100)*(100-100*exp(-rf*0.5)),0)
+  principal_guarantee_note_return <- principal_guarantee_note_CF/100 - 1
+  
+  print(underlying_price_return) #Print return of Fixed-coupon note
+  print(principal_guarantee_note_return) #Print return of Principal guarantee note
+  
+  summary <- setNames(data.frame(matrix(ncol = 2, nrow = 1)), c("Fixed-coupon note return", "Principal guarantee note return"))
+  summary[1] <- underlying_price_return
+  summary[2] <- principal_guarantee_note_return
+  View(summary)
+}
+
+#########################################################################################################
+#Backtest GSPC (i.e. market return)
+getSymbols("^GSPC", from = "2019-09-27", to = "2020-03-28") #load historical data
+GSPC_clean <- GSPC$GSPC.Adjusted[c("2019-09-27","2020-03-27")] #evaluation dates
+
+backtesting_function(GSPC_clean) #Call the backtesting function
+
+#########################################################################################################
+#Backtest GOOG
+getSymbols("GOOG", from = "2019-09-27", to = "2020-03-28") #load historical data
+GOOG_clean <- GOOG$GOOG.Adjusted[c("2019-09-27","2020-03-27")] #evaluation dates
+
+backtesting_function(GOOG_clean) #Call the backtesting function
+
+#########################################################################################################
+#Backtest FB
+getSymbols("FB", from = "2019-09-27", to = "2020-03-28") #load historical data
+FB_clean <- FB$FB.Adjusted[c("2019-09-27","2020-03-27")] #evaluation dates
+
+backtesting_function(FB_clean) #Call the backtesting function
+
+#########################################################################################################
+#Backtest AAPL
+getSymbols("AAPL", from = "2019-09-27", to = "2020-03-28") #load historical data
+AAPL_clean <- AAPL$AAPL.Adjusted[c("2019-09-27","2020-03-27")] #evaluation dates
+
+backtesting_function(AAPL_clean) #Call the backtesting function
